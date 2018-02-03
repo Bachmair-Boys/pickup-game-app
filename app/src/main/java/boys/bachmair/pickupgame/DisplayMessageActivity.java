aaef134a61419a81473daf7501975856117b3e49
Farhan Reagan <farhan.ahmed.1@stonybrook.edu>
2018-02-03 15:43:51 -0500

    working start screen

--- app/src/main/java/boys/bachmair/pickupgame/DisplayMessageActivity.java
+++ app/src/main/java/boys/bachmair/pickupgame/DisplayMessageActivity.java
@@ -1,9 +1,9 @@
 package boys.bachmair.pickupgame;
 
-import android.content.Intent;
+//import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+//import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
     @Override
@@ -11,11 +11,12 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        // Get the Intent that started this activity and extract the string
+        /*// Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.logintitle);
-        textView.setText(message);
+        textView.setText(message);*/
     }
+}
